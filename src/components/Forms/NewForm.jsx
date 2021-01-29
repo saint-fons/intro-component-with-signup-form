1d0dda915e41dd0bf9198b49d06b56b76644ba6b
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2021-01-29 16:49:06 +0300

    design ver 3

--- src/components/Forms/NewForm.jsx
+++ src/components/Forms/NewForm.jsx
@@ -24,92 +24,93 @@ const NewForm = () => {
                 </div>
             </div>
 
-            <div className={"FromEnterLogin"}>
+            <div className={"FormEnterLogin"}>
+                <div className={"FormLoginSide"}>
+                    <div className={"TryItFree"}>
+                        <span className={"WeightSpan"}>Try it free 7 days</span> then $20/mo. thereafter
+                    </div>
 
-                <div className={"TryItFree"}>
-                    <span className={"WeightSpan"}>Try it free 7 days</span> then $20/mo. thereafter
+                    <Styles>
+                        <Form
+                            onSubmit={onSubmit}
+                            validate={values => {
+                                const errors = {}
+                                if (!values.firstname) {
+                                    errors.firstname = 'Required'
+                                }
+                                if (!values.lastname) {
+                                    errors.lastname = 'Required'
+                                }
+                                if (!values.email) {
+                                    errors.email = 'Required'
+                                }
+                                if (!values.password) {
+                                    errors.password = 'Required'
+                                }
+                                return errors
+                            }}
+                            render={({
+                                         submitError,
+                                         handleSubmit,
+                                         form,
+                                         submitting,
+                                         pristine,
+                                         values
+                                     }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <Field name="firstname">
+                                        {({input, meta}) => (
+                                            <div>
+                                                <input {...input} type="text" placeholder="First Name"/>
+                                                {(meta.error || meta.submitError) && meta.touched && (
+                                                    <span>{meta.error || meta.submitError}</span>
+                                                )}
+                                            </div>
+                                        )}
+                                    </Field>
+                                    <Field name="lastname">
+                                        {({input, meta}) => (
+                                            <div>
+                                                <input {...input} type="text" placeholder="Last Name"/>
+                                                {(meta.error || meta.submitError) && meta.touched && (
+                                                    <span>{meta.error || meta.submitError}</span>
+                                                )}
+                                            </div>
+                                        )}
+                                    </Field>
+                                    <Field name="email">
+                                        {({input, meta}) => (
+                                            <div>
+                                                <input {...input} type="text" placeholder="Email Address"/>
+                                                {(meta.error || meta.submitError) && meta.touched && (
+                                                    <span>{meta.error || meta.submitError}</span>
+                                                )}
+                                            </div>
+                                        )}
+                                    </Field>
+                                    <Field name="password">
+                                        {({input, meta}) => (
+                                            <div>
+                                                <input {...input} type="password" placeholder="Password"/>
+                                                {meta.error && meta.touched && <span>{meta.error}</span>}
+                                            </div>
+                                        )}
+                                    </Field>
+                                    {submitError && <div className="error">{submitError}</div>}
+                                    <div className="buttons">
+                                        <button type="submit" disabled={submitting || pristine}>
+                                            CLAIM YOUR FREE TRIAL
+                                        </button>
+                                    </div>
+                                    <div className={"AgreeingTerms"}>
+                                        By clicking the button, you are agreeing to our <span
+                                        className={"TermsAndServices"}>Terms and Services</span>
+                                    </div>
+                                </form>
+                            )}
+                        />
+                    </Styles>
                 </div>
-
-                <Styles>
-                    <Form
-                        onSubmit={onSubmit}
-                        validate={values => {
-                            const errors = {}
-                            if (!values.firstname) {
-                                errors.firstname = 'Required'
-                            }
-                            if (!values.lastname) {
-                                errors.lastname = 'Required'
-                            }
-                            if (!values.email) {
-                                errors.email = 'Required'
-                            }
-                            if (!values.password) {
-                                errors.password = 'Required'
-                            }
-                            return errors
-                        }}
-                        render={({
-                                     submitError,
-                                     handleSubmit,
-                                     form,
-                                     submitting,
-                                     pristine,
-                                     values
-                                 }) => (
-                            <form onSubmit={handleSubmit}>
-                                <Field name="firstname">
-                                    {({input, meta}) => (
-                                        <div>
-                                            <input {...input} type="text" placeholder="First Name"/>
-                                            {(meta.error || meta.submitError) && meta.touched && (
-                                                <span>{meta.error || meta.submitError}</span>
-                                            )}
-                                        </div>
-                                    )}
-                                </Field>
-                                <Field name="lastname">
-                                    {({input, meta}) => (
-                                        <div>
-                                            <input {...input} type="text" placeholder="Last Name"/>
-                                            {(meta.error || meta.submitError) && meta.touched && (
-                                                <span>{meta.error || meta.submitError}</span>
-                                            )}
-                                        </div>
-                                    )}
-                                </Field>
-                                <Field name="email">
-                                    {({input, meta}) => (
-                                        <div>
-                                            <input {...input} type="text" placeholder="Email Address"/>
-                                            {(meta.error || meta.submitError) && meta.touched && (
-                                                <span>{meta.error || meta.submitError}</span>
-                                            )}
-                                        </div>
-                                    )}
-                                </Field>
-                                <Field name="password">
-                                    {({input, meta}) => (
-                                        <div>
-                                            <input {...input} type="password" placeholder="Password"/>
-                                            {meta.error && meta.touched && <span>{meta.error}</span>}
-                                        </div>
-                                    )}
-                                </Field>
-                                {submitError && <div className="error">{submitError}</div>}
-                                <div className="buttons">
-                                    <button type="submit" disabled={submitting || pristine}>
-                                        CLAIM YOUR FREE TRIAL
-                                    </button>
-                                </div>
-                                <div className={"AgreeingTerms"}>
-                                    By clicking the button, you are agreeing to our <span
-                                    className={"TermsAndServices"}>Terms and Services</span>
-                                </div>
-                            </form>
-                        )}
-                    />
-                </Styles>
             </div>
         </div>
     )
